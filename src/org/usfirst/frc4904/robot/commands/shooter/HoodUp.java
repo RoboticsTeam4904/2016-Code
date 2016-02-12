8e9da1d7e6460f8ac14544034764588c4488c78d
Yoni Lerner <yonilern@gmail.com>
2016-02-11 17:28:36 -0800

    Added Hood and commands

---
+++ src/org/usfirst/frc4904/robot/commands/shooter/HoodUp.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc4904.robot.commands.shooter;
+
+
+import org.usfirst.frc4904.robot.subsystems.Hood;
+
+public class HoodUp extends HoodSet {
+	public HoodUp(Hood hood) {
+		super("HoodUp", hood, true);
+	}
+}
---
+++ src/org/usfirst/frc4904/robot/subsystems/Hood.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc4904.robot.subsystems;
+
+
+import org.usfirst.frc4904.robot.commands.shooter.HoodUp;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Hood extends Subsystem {
+	protected boolean isUp;
+	protected final Solenoid solenoid;
+	
+	public Hood(Solenoid solenoid) {
+		super("Hood");
+		this.solenoid = solenoid;
+	}
+	
+	/**
+	 * Gets whether the hood is currently set to up.
+	 * Does not read the actual position - this is just the last requested position.
+	 * 
+	 * @return position
+	 *         true if the the current desired hood position is up.
+	 */
+	public boolean isUp() {
+		return isUp;
+	}
+	
+	/**
+	 * Gets whether the hood is currently set to down.
+	 * Does not read the actual position - this is just the last requested position.
+	 * 
+	 * @return position
+	 *         true if the the current desired hood position is down.
+	 */
+	public boolean isDown() {
+		return !isUp;
+	}
+	
+	/**
+	 * Sets the desired hood position.
+	 * True means up.
+	 * 
+	 * @param position
+	 *        True means up
+	 */
+	public void setPosition(boolean up) {
+		solenoid.set(up);
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(new HoodUp(this));
+	}
+}
