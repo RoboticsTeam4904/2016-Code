13a0df1a69834b68dc89f2dc164b7e1e65aa94fd
Arya Goutam <arygout@nuevaschool.org>
2016-02-16 14:40:49 -0800

    Removed Theoretical if statement from getDesiredFlywheelSpeed

--- src/org/usfirst/frc4904/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc4904/robot/subsystems/Shooter.java
@@ -13,7 +13,6 @@ public class Shooter extends Subsystem {
 	public final Hood hood;
 	public final Flywheel flywheel;
 	public final CANSensor distanceSensor;
-	protected final boolean useTheoreticalModel = false;
 	private final HashMap<Double, Double> measuredSpeeds;
 	
 	public Shooter(RockNRoller rockNRoller, Hood hood, Flywheel flywheel, CANSensor distanceSensor) {
@@ -27,26 +26,21 @@ public class Shooter extends Subsystem {
 	
 	public double getDesiredFlywheelSpeed() {
 		double distance = distanceSensor.read(0);
-		double speed;
-		if (useTheoreticalModel) {
-			// TODO add theoretical model code
-			speed = distance * 0.5; // Made up
-		} else {
-			double nearestDistance = nearestKey(distance, measuredSpeeds);
-			speed = measuredSpeeds.get(nearestDistance);
-		}
-		return speed;
+		double nearestDistance = nearestKey(distance, measuredSpeeds);
+		return measuredSpeeds.get(nearestDistance);
 	}
 	
 	/**
-	 * Find the nearest key to a given value in a HashMap with keys of type Double.
+	 * Find the nearest key to a given approximate key in a HashMap with keys of type Double.
 	 * Based off of http://stackoverflow.com/questions/519881/finding-closest-number-in-an-array
 	 *
-	 * @param value
+	 * @param approximateKey
+	 *        The approximate key. The key in the HashMap nearest to this will be returned.
 	 * @param map
-	 * @return The nearest value, or Double.NaNs if there is none (e.g. your HashMap is empty.)
+	 *        The HashMap to search the keys of. The keys must be of type Double.
+	 * @return The nearest key, or Double.NaN if there is none (e.g. your HashMap is empty.)
 	 */
-	protected double nearestKey(double value, HashMap<Double, ?> map) {
+	protected double nearestKey(double approximateKey, HashMap<Double, ?> map) {
 		Set<Double> keys = map.keySet();
 		double nearest = Double.NaN;
 		double bestDistanceFoundYet = Double.POSITIVE_INFINITY;
@@ -55,11 +49,11 @@ public class Shooter extends Subsystem {
 		while (i.hasNext()) {
 			double key = i.next();
 			// if we found the desired number, we return it.
-			if (key == value) {
+			if (key == approximateKey) {
 				return key;
 			} else {
 				// else, we consider the difference between the desired number and the current number in the array.
-				double d = Math.abs(value - key);
+				double d = Math.abs(approximateKey - key);
 				if (d < bestDistanceFoundYet) {
 					// For the moment, this value is the nearest to the desired number...
 					bestDistanceFoundYet = d; // Assign new best distance...
