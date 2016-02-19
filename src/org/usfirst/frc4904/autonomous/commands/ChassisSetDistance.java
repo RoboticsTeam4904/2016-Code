e744521cff70f23bb009d1a55b35dca4fa61efa3
Thomas the Dank Engine <thomas.the@dank.engines>
2016-02-19 12:04:34 -0800

    Nacho pls no cancer

--- src/org/usfirst/frc4904/autonomous/commands/ChassisSetDistance.java
+++ src/org/usfirst/frc4904/autonomous/commands/ChassisSetDistance.java
@@ -1,25 +1,22 @@
 package org.usfirst.frc4904.autonomous.commands;
 
-import java.util.ArrayList;
+
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.motor.MotorSet;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import org.usfirst.frc4904.standard.subsystems.motor.SensorMotor;
-
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class ChassisSetDistance extends CommandGroup{
-	
+public class ChassisSetDistance extends CommandGroup {
 	protected CustomEncoder[] motorEncoders;
 	protected MotorSet[] motorSetCommands;
 	protected Chassis chassis;
 	protected double distance;
 	protected double speed;
 	
-	public ChassisSetDistance(Chassis chassis, double distance, double speed, boolean usePID, CustomEncoder... customEncoders){
+	public ChassisSetDistance(Chassis chassis, double distance, double speed, boolean usePID, CustomEncoder... customEncoders) {
 		requires(chassis);
 		this.chassis = chassis;
 		this.distance = distance;
@@ -27,7 +24,6 @@ public class ChassisSetDistance extends CommandGroup{
 		Motor[] chassisMotors = chassis.getMotors();
 		motorSetCommands = new MotorSet[chassisMotors.length];
 		motorEncoders = customEncoders;
-		
 		for (int i = 0; i < chassisMotors.length; i++) {
 			if (chassisMotors[i] instanceof SensorMotor) {
 				if (usePID) {
@@ -40,22 +36,24 @@ public class ChassisSetDistance extends CommandGroup{
 			addParallel(motorSetCommands[i]);
 		}
 	}
-
+	
+	@Override
 	public void execute() {
 		chassis.move2dc(0.0, speed, 0.0);
 		double[] chassisMotorSpeeds = chassis.getMotorSpeeds();
-		for(int i = 0; i < motorSetCommands.length; i++) {
+		for (int i = 0; i < motorSetCommands.length; i++) {
 			motorSetCommands[i].set(chassisMotorSpeeds[i]);
 		}
 	}
-
+	
+	@Override
 	public boolean isFinished() {
 		double distanceSum = 0;
 		for (int i = 0; i < motorEncoders.length; i++) {
 			distanceSum += motorEncoders[i].getDistance();
+			LogKitten.wtf("Encoder " + i + " reads " + motorEncoders[i].getDistance() + "out of " + distance);
 		}
 		double distanceAvg = distanceSum / motorEncoders.length;
 		return distanceAvg > distance;
 	}
-
 }
--- src/org/usfirst/frc4904/autonomous/commands/CrossLowbar.java
+++ src/org/usfirst/frc4904/autonomous/commands/CrossLowbar.java
@@ -3,6 +3,7 @@ package org.usfirst.frc4904.autonomous.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
+import org.usfirst.frc4904.standard.commands.RunFor;
 import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
@@ -14,9 +15,11 @@ public class CrossLowbar extends CommandGroup {
 	public CrossLowbar(Chassis chassis, boolean usePID) {
 		encoders = new CustomEncoder[2];
 		encoders[0] = new CANEncoder(RobotMap.Port.CAN.leftEncoder);
+		encoders[0].setReverseDirection(true);
 		encoders[1] = new CANEncoder(RobotMap.Port.CAN.rightEncoder);
+		encoders[1].setReverseDirection(false);
 		double tickDistance = (RobotMap.Constant.FieldMetric.DISTANCE_TO_LOW_BAR / RobotMap.Constant.RobotMetric.WHEEL_CIRCUMFERENCE) * RobotMap.Constant.RobotMetric.WHEEL_ENCODER_PPR;
-		addSequential(new HoodDown(RobotMap.Component.hood));
+		addSequential(new RunFor(new HoodDown(RobotMap.Component.hood), 1));
 		addSequential(new ChassisSetDistance(chassis, tickDistance, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, usePID, encoders));
 	}
 }
