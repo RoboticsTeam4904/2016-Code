e974cb46cd54f3186fceeb61616537893c15268c
nacgarg <nachiketa.gargi@gmail.com>
2016-02-19 11:06:15 -0800

    Added HoodDown to CrossLowbar

--- src/org/usfirst/frc4904/autonomous/commands/CrossLowbar.java
+++ src/org/usfirst/frc4904/autonomous/commands/CrossLowbar.java
@@ -2,6 +2,7 @@ package org.usfirst.frc4904.autonomous.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
 import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
@@ -9,12 +10,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class CrossLowbar extends CommandGroup {
 	protected final CustomEncoder[] encoders;
-
+	
 	public CrossLowbar(Chassis chassis, boolean usePID) {
 		encoders = new CustomEncoder[2];
 		encoders[0] = new CANEncoder(RobotMap.Port.CAN.leftEncoder);
 		encoders[1] = new CANEncoder(RobotMap.Port.CAN.rightEncoder);
 		double tickDistance = (RobotMap.Constant.Autonomous.DISTANCE_TO_LOW_BAR / RobotMap.Constant.RobotMetrics.WHEEL_CIRCUMFERENCE) * RobotMap.Constant.RobotMetrics.WHEEL_ENCODER_PPR;
+		addSequential(new HoodDown(RobotMap.Component.hood));
 		addSequential(new ChassisSetDistance(chassis, tickDistance, RobotMap.Constant.Autonomous.DRIVE_SPEED, usePID, encoders));
 	}
 }
