7edf3c18999de1ad7c0650e5250c0ddbaa5786f3
Yoni Lerner <yonilern@gmail.com>
2016-02-19 18:38:22 -0800

    Made Hood auto-open on SpinUpFlywheel

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -2,11 +2,11 @@ package org.usfirst.frc4904.robot.humaninterface.operators;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
 import org.usfirst.frc4904.robot.commands.shooter.HoodUp;
 import org.usfirst.frc4904.robot.commands.shooter.RockNRollerOuttake;
 import org.usfirst.frc4904.robot.commands.shooter.RockNRollerShoot;
 import org.usfirst.frc4904.robot.commands.shooter.SpinUpFlywheel;
+import org.usfirst.frc4904.standard.commands.RunAllSequential;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.standard.custom.controllers.Controller;
@@ -19,11 +19,9 @@ public class DefaultOperator extends Operator {
 	
 	@Override
 	public void bindCommands() {
-		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheel());
 		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new RockNRollerShoot());
+		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new RunAllSequential(new HoodUp(), new SpinUpFlywheel()));
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new MotorConstant(RobotMap.Component.intakeRoller, -1));
-		RobotMap.HumanInput.Operator.stick.button3.whenPressed(new HoodDown(RobotMap.Component.hood));
-		RobotMap.HumanInput.Operator.stick.button5.whenPressed(new HoodUp(RobotMap.Component.hood));
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new RockNRollerOuttake());
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileReleased(new MotorControl(RobotMap.Component.intakeRoller, RobotMap.HumanInput.Operator.stick, Controller.Y_AXIS, false));
 	}
