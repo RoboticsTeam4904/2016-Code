75697d648e607d98e00c73f6ee8046d40a7bc9f3
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-19 22:29:41 -0800

    Modified ChassisSetDistance to use ChassisConstant.

--- src/org/usfirst/frc4904/autonomous/commands/ChassisSetDistance.java
+++ src/org/usfirst/frc4904/autonomous/commands/ChassisSetDistance.java
@@ -2,48 +2,17 @@ package org.usfirst.frc4904.autonomous.commands;
 
 
 import org.usfirst.frc4904.standard.LogKitten;
-import org.usfirst.frc4904.standard.commands.motor.MotorSet;
+import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
-import org.usfirst.frc4904.standard.subsystems.motor.Motor;
-import org.usfirst.frc4904.standard.subsystems.motor.SensorMotor;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class ChassisSetDistance extends CommandGroup {
+public class ChassisSetDistance extends ChassisConstant {
 	protected CustomEncoder[] motorEncoders;
-	protected MotorSet[] motorSetCommands;
-	protected Chassis chassis;
 	protected double distance;
-	protected double speed;
 	
 	public ChassisSetDistance(Chassis chassis, double distance, double speed, boolean usePID, CustomEncoder... customEncoders) {
+		super(chassis, 0.0, speed, 0.0, Double.MAX_VALUE);
 		requires(chassis);
-		this.chassis = chassis;
-		this.distance = distance;
-		this.speed = speed;
-		Motor[] chassisMotors = chassis.getMotors();
-		motorSetCommands = new MotorSet[chassisMotors.length];
-		motorEncoders = customEncoders;
-		for (int i = 0; i < chassisMotors.length; i++) {
-			if (chassisMotors[i] instanceof SensorMotor) {
-				if (usePID) {
-					((SensorMotor) chassisMotors[i]).enablePID();
-				} else {
-					((SensorMotor) chassisMotors[i]).disablePID();
-				}
-			}
-			motorSetCommands[i] = new MotorSet(chassisMotors[i]);
-			addParallel(motorSetCommands[i]);
-		}
-	}
-	
-	@Override
-	public void execute() {
-		chassis.move2dc(0.0, speed, 0.0);
-		double[] chassisMotorSpeeds = chassis.getMotorSpeeds();
-		for (int i = 0; i < motorSetCommands.length; i++) {
-			motorSetCommands[i].set(chassisMotorSpeeds[i]);
-		}
 	}
 	
 	@Override
