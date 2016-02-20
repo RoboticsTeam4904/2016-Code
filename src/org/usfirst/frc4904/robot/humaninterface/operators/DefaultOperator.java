c9d34beae6d61705f53534213f260cfdc8c43060
AJ <aj@ajnadel.com>
2016-02-19 17:36:15 -0800

    add safety checks!

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -5,7 +5,8 @@ import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
 import org.usfirst.frc4904.robot.commands.shooter.HoodUp;
 import org.usfirst.frc4904.robot.commands.shooter.RockNRollerOuttake;
-import org.usfirst.frc4904.robot.commands.shooter.RockNRollerShoot;
+import org.usfirst.frc4904.robot.commands.shooter.Shoot;
+import org.usfirst.frc4904.robot.commands.shooter.SpinUpFlywheel;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.standard.custom.controllers.Controller;
@@ -18,8 +19,8 @@ public class DefaultOperator extends Operator {
 	
 	@Override
 	public void bindCommands() {
-		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new RockNRollerShoot(RobotMap.Component.rockNRoller));
-		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new MotorConstant(RobotMap.Component.flywheel, 1));
+		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new Shoot());
+		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheel());
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new MotorConstant(RobotMap.Component.intakeRoller, -1));
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new RockNRollerOuttake(RobotMap.Component.rockNRoller));
 		RobotMap.HumanInput.Operator.stick.button3.whenPressed(new HoodDown(RobotMap.Component.hood));
