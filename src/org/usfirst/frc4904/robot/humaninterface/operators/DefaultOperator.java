da1a04b71909506cea27f090c64826ec45ff2dba
Yoni Lerner <yonilern@gmail.com>
2016-02-19 18:38:01 -0800

    Removed Shoot

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -5,7 +5,7 @@ import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
 import org.usfirst.frc4904.robot.commands.shooter.HoodUp;
 import org.usfirst.frc4904.robot.commands.shooter.RockNRollerOuttake;
-import org.usfirst.frc4904.robot.commands.shooter.Shoot;
+import org.usfirst.frc4904.robot.commands.shooter.RockNRollerShoot;
 import org.usfirst.frc4904.robot.commands.shooter.SpinUpFlywheel;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
@@ -19,8 +19,8 @@ public class DefaultOperator extends Operator {
 	
 	@Override
 	public void bindCommands() {
-		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new Shoot());
 		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheel());
+		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new RockNRollerShoot());
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new MotorConstant(RobotMap.Component.intakeRoller, -1));
 		RobotMap.HumanInput.Operator.stick.button3.whenPressed(new HoodDown(RobotMap.Component.hood));
 		RobotMap.HumanInput.Operator.stick.button5.whenPressed(new HoodUp(RobotMap.Component.hood));
