0bb29997b8bb0f14a80a6b8a55355c898c016776
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-20 12:39:04 -0800

    Automatic hood.

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -7,9 +7,11 @@ import org.usfirst.frc4904.robot.commands.shooter.HoodUp;
 import org.usfirst.frc4904.robot.commands.shooter.Outtake;
 import org.usfirst.frc4904.robot.commands.shooter.RockNRollerShoot;
 import org.usfirst.frc4904.robot.commands.shooter.SpinUpFlywheel;
+import org.usfirst.frc4904.standard.commands.RunAllSequential;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.standard.custom.controllers.Controller;
 import org.usfirst.frc4904.standard.humaninput.Operator;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class DefaultOperator extends Operator {
 	public DefaultOperator() {
@@ -19,10 +21,11 @@ public class DefaultOperator extends Operator {
 	@Override
 	public void bindCommands() {
 		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new RockNRollerShoot());
-		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheel());
+		RobotMap.HumanInput.Operator.stick.button2.whenPressed(new HoodUp());
+		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new RunAllSequential(new WaitCommand(0.25), new SpinUpFlywheel()));
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new Outtake());
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileReleased(new MotorControl(RobotMap.Component.intakeRoller, RobotMap.HumanInput.Operator.stick, Controller.Y_AXIS, false));
-		RobotMap.HumanInput.Operator.stick.button3.whenPressed(new HoodDown());
 		RobotMap.HumanInput.Operator.stick.button5.whenPressed(new HoodUp());
+		RobotMap.HumanInput.Operator.stick.button3.whenPressed(new HoodDown());
 	}
 }
