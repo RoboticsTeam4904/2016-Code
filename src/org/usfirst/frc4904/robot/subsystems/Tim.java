3a53441ac982662b703db51b1c6b7b02ca894c47
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-20 22:30:30 -0800

    Added safe set to Tim.

--- src/org/usfirst/frc4904/robot/subsystems/Tim.java
+++ src/org/usfirst/frc4904/robot/subsystems/Tim.java
@@ -5,6 +5,7 @@ import org.usfirst.frc4904.robot.RobotMap.Component;
 import org.usfirst.frc4904.robot.RobotMap.Constant;
 import org.usfirst.frc4904.robot.commands.shooter.TimDefault;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
+import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.motor.PositionEncodedMotor;
 import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.AccelerationCap;
 import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.LinearModifier;
@@ -14,15 +15,28 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class Tim extends PositionEncodedMotor {
 	public static final double TIM_FULL_UP = 50;
 	public static final double TIM_FULL_DOWN = 2150;
+	private final CustomEncoder encoder;
 	
-	public Tim(MotionController motionController, SpeedController... motors) {
+	public Tim(MotionController motionController, CustomEncoder encoder, SpeedController... motors) {
 		super("Tim", new SpeedModifierGroup(new LinearModifier(Constant.HumanInput.DEFENSE_MANIPULATOR_SPEED_SCALE), new AccelerationCap(Component.pdp)), motionController, motors);
+		this.encoder = encoder;
 	}
 	
 	@Override
 	public void setPosition(double position) {
 		double safePosition = Math.max(Math.min(position, Tim.TIM_FULL_DOWN), Tim.TIM_FULL_UP);
 		super.setPosition(safePosition);
+	}
+	
+	@Override
+	public void set(double speed) {
+		if (encoder.get() < Tim.TIM_FULL_UP && speed < 0) {
+			return;
+		}
+		if (encoder.get() > Tim.TIM_FULL_DOWN && speed > 0) {
+			return;
+		}
+		super.set(speed);
 	}
 	
 	@Override
