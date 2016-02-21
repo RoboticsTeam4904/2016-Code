e5e8efc6f2f478c0f778e3cca5b7273e69db27b1
Thomas the Dank Engine <thomas.the@dank.engines>
2016-02-20 21:11:12 -0800

    Started open portcullis

---
+++ src/org/usfirst/frc4904/autonomous/commands/TimOpenPortcullis.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc4904.autonomous.commands;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class TimOpenPortcullis extends CommandGroup {
+	public TimOpenPortcullis() {
+		// addSequential(new MotorConstantPosition(RobotMap.Component.tim, Tim.TIM_FULL_DOWN));
+	}
+}
