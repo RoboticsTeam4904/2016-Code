dfa09c1386c741d6fced1beae9c36263e8198ad0
Carter Turnbaugh <carturn@nuevaschool.org>
2016-03-25 10:22:04 -0700

    Made all strategies start using TimLowbar.

--- src/org/usfirst/frc4904/autonomous/strategies/CrossMoatTime.java
+++ src/org/usfirst/frc4904/autonomous/strategies/CrossMoatTime.java
@@ -2,10 +2,9 @@ package org.usfirst.frc4904.autonomous.strategies;
 
 
 import org.usfirst.frc4904.autonomous.commands.AutonomousMoveTime;
+import org.usfirst.frc4904.autonomous.commands.TimLowbar;
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.subsystems.Tim;
 import org.usfirst.frc4904.standard.commands.RunAllSequential;
-import org.usfirst.frc4904.standard.commands.motor.MotorPositionConstant;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -13,6 +12,6 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class CrossMoatTime extends CommandGroup {
 	public CrossMoatTime(Chassis chassis, boolean usePID) {
 		addParallel(new RunAllSequential(new WaitCommand(0.5), new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.SPEED_MOAT, RobotMap.Constant.AutonomousMetric.TIME_MOAT, usePID)));
-		addParallel(new MotorPositionConstant(RobotMap.Component.tim, (Tim.TIM_FULL_DOWN - Tim.TIM_FULL_UP) / 2));
+		addParallel(new TimLowbar(RobotMap.Component.tim, RobotMap.Component.timEncoder));
 	}
 }
--- src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
+++ src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
@@ -2,10 +2,9 @@ package org.usfirst.frc4904.autonomous.strategies;
 
 
 import org.usfirst.frc4904.autonomous.commands.AutonomousMoveTime;
+import org.usfirst.frc4904.autonomous.commands.TimLowbar;
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.subsystems.Tim;
 import org.usfirst.frc4904.standard.commands.RunAllSequential;
-import org.usfirst.frc4904.standard.commands.motor.MotorPositionConstant;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -14,6 +13,6 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class CrossRockwallTime extends CommandGroup {
 	public CrossRockwallTime(Chassis chassis, boolean usePID) {
 		addParallel(new RunAllSequential(new WaitCommand(0.5), new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, RobotMap.Constant.AutonomousMetric.TIME_ROCK_WALL, usePID)));
-		addParallel(new MotorPositionConstant(RobotMap.Component.tim, (Tim.TIM_FULL_DOWN - Tim.TIM_FULL_UP) / 2));
+		addParallel(new TimLowbar(RobotMap.Component.tim, RobotMap.Component.timEncoder));
 	}
 }
