cf2056381bb60d96a838b5b8cbf998c5055307a6
Carter Turnbaugh <carturn@nuevaschool.org>
2016-03-24 17:33:24 -0700

    Fixed Tim moving for all auton commands.

--- src/org/usfirst/frc4904/autonomous/strategies/CrossLowbarDistance.java
+++ src/org/usfirst/frc4904/autonomous/strategies/CrossLowbarDistance.java
@@ -4,9 +4,13 @@ package org.usfirst.frc4904.autonomous.strategies;
 import org.usfirst.frc4904.autonomous.commands.AutonomousMoveDistance;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.shooter.HoodDown;
+import org.usfirst.frc4904.robot.subsystems.Tim;
+import org.usfirst.frc4904.standard.commands.RunAllSequential;
+import org.usfirst.frc4904.standard.commands.motor.MotorPositionConstant;
 import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class CrossLowbarDistance extends CommandGroup {
 	public CrossLowbarDistance(Chassis chassis, boolean usePID) {
@@ -14,6 +18,7 @@ public class CrossLowbarDistance extends CommandGroup {
 		addParallel(new HoodDown());
 		RobotMap.Component.leftWheelEncoder.reset();
 		RobotMap.Component.rightWheelEncoder.reset();
-		addSequential(new AutonomousMoveDistance(chassis, tickDistance, RobotMap.Constant.AutonomousMetric.SPEED_LOWBAR, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder}));
+		addSequential(new MotorPositionConstant(RobotMap.Component.tim, (Tim.TIM_FULL_DOWN - Tim.TIM_FULL_UP) / 2));
+		addParallel(new RunAllSequential(new WaitCommand(0.5), new AutonomousMoveDistance(chassis, tickDistance, RobotMap.Constant.AutonomousMetric.SPEED_LOWBAR, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder})));
 	}
 }
--- src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
+++ src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
@@ -4,14 +4,16 @@ package org.usfirst.frc4904.autonomous.strategies;
 import org.usfirst.frc4904.autonomous.commands.AutonomousMoveTime;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.Tim;
+import org.usfirst.frc4904.standard.commands.RunAllSequential;
 import org.usfirst.frc4904.standard.commands.motor.MotorPositionConstant;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 @Deprecated
 public class CrossRockwallTime extends CommandGroup {
 	public CrossRockwallTime(Chassis chassis, boolean usePID) {
-		addSequential(new MotorPositionConstant(RobotMap.Component.tim, Tim.TIM_FULL_DOWN));
-		addSequential(new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, RobotMap.Constant.AutonomousMetric.TIME_ROCK_WALL, usePID));
+		addSequential(new MotorPositionConstant(RobotMap.Component.tim, (Tim.TIM_FULL_DOWN - Tim.TIM_FULL_UP) / 2));
+		addParallel(new RunAllSequential(new WaitCommand(0.5), new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, RobotMap.Constant.AutonomousMetric.TIME_ROCK_WALL, usePID)));
 	}
 }
