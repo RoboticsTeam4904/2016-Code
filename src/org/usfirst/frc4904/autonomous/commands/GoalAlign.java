7a0202962c5b431d09b936035a7aaa2cd9c9fb09
HowardStark <howard@getcoffee.io>
2016-03-25 11:20:33 -0700

    Fixed reference to old data system

--- src/org/usfirst/frc4904/autonomous/commands/GoalAlign.java
+++ src/org/usfirst/frc4904/autonomous/commands/GoalAlign.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class GoalAlign extends CommandGroup {
 	public GoalAlign(Chassis chassis, Camera camera, boolean usePID) {
-		addSequential(new ChassisTurnDegrees(chassis, RobotMap.Constant.RobotMetric.LENGTH, RobotMap.Constant.RobotMetric.WIDTH, RobotMap.Constant.RobotMetric.WHEEL_ENCODER_PPR, RobotMap.Constant.RobotMetric.WHEEL_DIAMETER, RobotMap.Component.cameraIR.getGoalOffAngle(true), RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder}));
-		addSequential(new AutonomousMoveDistance(chassis, RobotMap.Component.cameraIR.getGoalOffDistance(true), RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder}));
+		addSequential(new ChassisTurnDegrees(chassis, RobotMap.Constant.RobotMetric.LENGTH, RobotMap.Constant.RobotMetric.WIDTH, RobotMap.Constant.RobotMetric.WHEEL_ENCODER_PPR, RobotMap.Constant.RobotMetric.WHEEL_DIAMETER, RobotMap.Component.cameraIR.getCameraData(true).getDegreesToTurn(), RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder}));
+		addSequential(new AutonomousMoveDistance(chassis, RobotMap.Component.cameraIR.getCameraData(true).getDistanceToMove(), RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, usePID, new CustomEncoder[] {RobotMap.Component.leftWheelEncoder, RobotMap.Component.rightWheelEncoder}));
 	}
 }
