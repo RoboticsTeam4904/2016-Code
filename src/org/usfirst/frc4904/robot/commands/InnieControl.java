16a56bc1a864c37a47ccdf5cd35b7c32df580f20
AJ <aj@ajnadel.com>
2016-04-07 14:40:17 -0700

    consilidate into TimIntake, and change InnieControl

--- src/org/usfirst/frc4904/robot/commands/InnieControl.java
+++ src/org/usfirst/frc4904/robot/commands/InnieControl.java
@@ -3,7 +3,6 @@ package org.usfirst.frc4904.robot.commands;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.RockNRoller.RockerState;
-import org.usfirst.frc4904.robot.subsystems.Tim;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.standard.custom.controllers.Controller;
 
@@ -11,16 +10,14 @@ import org.usfirst.frc4904.standard.custom.controllers.Controller;
  * Continously control the intake roller with a controller,
  * 
  * @see MotorControl
- * 
+ * 		
  */
 public class InnieControl extends MotorControl {
-	protected final TimIntake timIntakeSpin;
-	protected final TimSet timIntakePosition;
+	protected final TimIntake timIntake;
 	
 	public InnieControl() {
 		super(RobotMap.Component.innie, RobotMap.HumanInput.Operator.stick, Controller.Y_AXIS, false);
-		timIntakeSpin = new TimIntake();
-		timIntakePosition = new TimSet(Tim.TimState.INTAKE);
+		timIntake = new TimIntake();
 	}
 	
 	@Override
@@ -29,21 +26,18 @@ public class InnieControl extends MotorControl {
 		boolean isDirectionIntake = (speed >= 0) && !invert;
 		if (isDirectionIntake) {
 			if (RobotMap.Component.shooter.isBallLoaded() && !RobotMap.Component.shooter.ballLoadOverride) {
-				timIntakePosition.cancel();
-				timIntakeSpin.cancel();
+				timIntake.cancel();
 				motor.set(RobotMap.Constant.INNIE_BALL_HOLD_SPEED);
 			} else {
 				super.execute(); // run Innie from joystick (a la MotorControl)
 				if (speed > RobotMap.Constant.HumanInput.TIM_DOWN_INTAKE_SPEED_THRESHOLD) {
-					timIntakeSpin.start();
-					timIntakePosition.start();
+					timIntake.start();
 				} else {
-					timIntakeSpin.cancel();
-					timIntakePosition.cancel();
+					timIntake.cancel();
 				}
 			}
 		} else { // outtaking
-			timIntakeSpin.cancel();
+			timIntake.cancel();
 			if (speed > RobotMap.Constant.HumanInput.OPERATOR_Y_OUTTAKE_UPPER_THRESHOLD) { // if not past threshold
 				stopMotors();
 				return;
--- src/org/usfirst/frc4904/robot/commands/TimIntake.java
+++ src/org/usfirst/frc4904/robot/commands/TimIntake.java
@@ -2,10 +2,14 @@ package org.usfirst.frc4904.robot.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.subsystems.Tim;
 import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class TimIntake extends MotorConstant {
+public class TimIntake extends CommandGroup {
 	public TimIntake() {
-		super(RobotMap.Component.tim.intakeMotor, RobotMap.Constant.TIM_INTAKE_SPEED);
+		super();
+		addParallel(new MotorConstant(RobotMap.Component.tim.intakeMotor, RobotMap.Constant.TIM_INTAKE_SPEED));
+		addParallel(new TimSet(Tim.TimState.INTAKE, false));
 	}
 }
