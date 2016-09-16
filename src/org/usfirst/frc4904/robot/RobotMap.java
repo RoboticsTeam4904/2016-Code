581789bebb0ac0cf871fddbc2106b928d5f36a25
Carter Turnbaugh <carturn@nuevaschool.org>
2016-09-16 09:38:16 -0700

    Tim is less agressive

--- src/org/usfirst/frc4904/robot/RobotMap.java
+++ src/org/usfirst/frc4904/robot/RobotMap.java
@@ -174,7 +174,7 @@ public class RobotMap {
 		public static final double TIMTAP_DURATION = 1;
 		public static final double TIM_INTAKE_SPEED = 0.75;
 		public static final double INNIE_SHOOT_SPEED = 1;
-		public static final double TIM_P = 0.001;
+		public static final double TIM_P = 0.0008;
 		public static final double TIM_I = -0.000;
 		public static final double TIM_D = -0.001;
 		public static final double TIM_ABSOLUTE_TOLERANCE = 50;
