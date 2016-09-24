2e59f57f3a0d1a435c04364bf04468593029648d
Yoni Lerner <yonilern@gmail.com>
2016-09-23 23:45:37 -0700

    Added shooting counterparts to all autonomous strategies

--- src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
+++ src/org/usfirst/frc4904/autonomous/strategies/CrossRockwallTime.java
@@ -3,14 +3,12 @@ package org.usfirst.frc4904.autonomous.strategies;
 
 import org.usfirst.frc4904.autonomous.commands.AutonomousMoveTime;
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.standard.commands.RunAllSequential;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
 
 @Deprecated
 public class CrossRockwallTime extends CommandGroup {
 	public CrossRockwallTime(Chassis chassis, boolean usePID) {
-		addParallel(new RunAllSequential(new WaitCommand(0.5), new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, RobotMap.Constant.AutonomousMetric.TIME_ROCK_WALL, usePID)));
+		addSequential(new AutonomousMoveTime(chassis, RobotMap.Constant.AutonomousMetric.DRIVE_SPEED, RobotMap.Constant.AutonomousMetric.TIME_ROCK_WALL, usePID));
 	}
 }
--- src/org/usfirst/frc4904/autonomous/strategies/Shoot.java
+++ src/org/usfirst/frc4904/autonomous/strategies/Shoot.java
@@ -4,15 +4,14 @@ package org.usfirst.frc4904.autonomous.strategies;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.RockNRollerShoot;
 import org.usfirst.frc4904.robot.commands.SpinUpFlywheel;
-import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
+import org.usfirst.frc4904.standard.commands.RunAllSequential;
+import org.usfirst.frc4904.standard.commands.RunFor;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class Shoot extends CommandGroup {
 	public Shoot() {
-		addParallel(new SpinUpFlywheel());
-		addSequential(new WaitCommand(RobotMap.Constant.AutonomousMetric.FLYWHEEL_SPINUP_DURATION));
-		addSequential(new RockNRollerShoot());
-		addParallel(new MotorIdle(RobotMap.Component.flywheel));
+		addParallel(new RunFor(new SpinUpFlywheel(), RobotMap.Constant.AutonomousMetric.FLYWHEEL_SPINUP_DURATION + 1)); // spin up the flywheel (an extra second after the shot)
+		addSequential(new RunAllSequential(new WaitCommand(RobotMap.Constant.AutonomousMetric.FLYWHEEL_SPINUP_DURATION), new RockNRollerShoot())); // wait for the flywheel to spin up and then shoot
 	}
 }
