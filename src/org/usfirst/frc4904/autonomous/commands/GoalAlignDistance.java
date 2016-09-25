b2792642b0fc907c21c72826f3115cf14df3ff0e
Yoni Lerner <yonilern@gmail.com>
2016-09-24 21:51:10 -0700

    Changed GoalAlignDistance to use encoder

--- src/org/usfirst/frc4904/autonomous/commands/GoalAlignDistance.java
+++ src/org/usfirst/frc4904/autonomous/commands/GoalAlignDistance.java
@@ -2,29 +2,29 @@ package org.usfirst.frc4904.autonomous.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.subsystems.Camera;
-import org.usfirst.frc4904.robot.subsystems.CameraPIDSource;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisMove;
 import org.usfirst.frc4904.standard.custom.ChassisController;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.CustomPIDController;
+import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class GoalAlignDistance extends Command implements ChassisController {
 	private final Chassis chassis;
-	private final Camera camera;
+	private final CustomEncoder encoder;
 	private final boolean stopWhenOnTarget;
 	private boolean isDistanceAligned;
 	private ChassisMove chassisMove;
 	private final CustomPIDController pidController;
 	
-	public GoalAlignDistance(Chassis chassis, Camera camera, boolean stopWhenOnTarget) {
-		this.camera = camera;
+	public GoalAlignDistance(Chassis chassis, CustomEncoder encoder, boolean stopWhenOnTarget) {
+		this.encoder = encoder;
 		this.chassis = chassis;
 		this.stopWhenOnTarget = stopWhenOnTarget;
 		isDistanceAligned = false;
-		pidController = new CustomPIDController(RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_P, RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_I, RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_D, new CameraPIDSource(camera, PIDSourceType.kDisplacement));
+		encoder.setPIDSourceType(PIDSourceType.kDisplacement);
+		pidController = new CustomPIDController(RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_P, RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_I, RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_D, encoder);
 		pidController.setAbsoluteTolerance(RobotMap.Constant.AutonomousMetric.DISTANCE_ALIGN_TOLERANCE);
 		pidController.setOutputRange(-1, 1);
 		pidController.enable();
@@ -39,12 +39,8 @@ public class GoalAlignDistance extends Command implements ChassisController {
 	@Override
 	public double getY() {
 		double get = pidController.get();
-		if (camera.getCameraData().canSeeGoal()) {
-			isDistanceAligned = pidController.onTarget();
-			return get;
-		} else {
-			return 0;
-		}
+		isDistanceAligned = pidController.onTarget();
+		return get;
 	}
 	
 	@Override
