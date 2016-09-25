596cbddeed5a99faaf51255f97823bd6995147b8
Yoni Lerner <yonilern@gmail.com>
2016-09-24 21:42:13 -0700

    Formatted SmartDashboard Keys and removed unused

--- src/org/usfirst/frc4904/robot/SmartDashboardKey.java
+++ src/org/usfirst/frc4904/robot/SmartDashboardKey.java
@@ -2,7 +2,12 @@ package org.usfirst.frc4904.robot;
 
 
 public enum SmartDashboardKey {
-	SHOOT_READY("Shoot!"), BATTER_END_OF_MATCH_TURN("20 seconds left"), FLYWHEEL_STATE("Flywheel spun up"), ANGLE_OFF_GOAL("Angle off Goal"), IN_RANGE("In Range"), DISTANCE_FROM_GOAL("Distance From Goal"), ANGLE_ALIGNED("Angle aligned"), AUTON_ROUTINE("Auton Routine"), SUBSYSTEM_SUMMARY("Subsystem summary");
+	ANGLE_OFF_GOAL("Angle off Goal"), //
+	DISTANCE_FROM_GOAL("Distance From Goal"), //
+	ANGLE_ALIGNED("Angle aligned"), //
+	DISTANCE_ALIGNED("Distance aligned"), //
+	SHOOT_READY("Shoot!"), //
+	SUBSYSTEM_SUMMARY("Subsystem summary"); //
 	public final String key;
 	
 	private SmartDashboardKey(String key) {
