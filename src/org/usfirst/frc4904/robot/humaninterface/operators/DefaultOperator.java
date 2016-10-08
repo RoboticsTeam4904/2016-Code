f35a3e133f8ffdf6517d99cb7e6f9f0c62d992c3
Carter Turnbaugh <carturn@nuevaschool.org>
2016-10-08 10:02:30 -0700

    Made operator use new flywheel sometimes end

--- src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -4,7 +4,7 @@ package org.usfirst.frc4904.robot.humaninterface.operators;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.commands.InnieControl;
 import org.usfirst.frc4904.robot.commands.Outtake;
-import org.usfirst.frc4904.robot.commands.ShootDistance;
+import org.usfirst.frc4904.robot.commands.RockNRollerShoot;
 import org.usfirst.frc4904.robot.commands.SpinUpFlywheelDistance;
 import org.usfirst.frc4904.standard.humaninput.Operator;
 
@@ -15,8 +15,8 @@ public class DefaultOperator extends Operator {
 	
 	@Override
 	public void bindCommands() {
-		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new ShootDistance());
-		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheelDistance());
+		RobotMap.HumanInput.Operator.stick.button1.onlyWhileHeld(new RockNRollerShoot());
+		RobotMap.HumanInput.Operator.stick.button2.onlyWhileHeld(new SpinUpFlywheelDistance(false));
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileHeld(new Outtake());
 		RobotMap.HumanInput.Operator.stick.button4.onlyWhileReleased(new InnieControl());
 	}
