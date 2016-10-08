f71c0c4454a9b2352bd0c317c7337eed7678f2a1
Carter Turnbaugh <carturn@nuevaschool.org>
2016-10-08 15:14:31 -0700

    Added function to calculate power form Ultrasonic Sensor

--- src/org/usfirst/frc4904/robot/commands/SpinUpFlywheelDistance.java
+++ src/org/usfirst/frc4904/robot/commands/SpinUpFlywheelDistance.java
@@ -2,32 +2,39 @@ package org.usfirst.frc4904.robot.commands;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.custom.sensors.DistanceSensor;
 import org.usfirst.frc4904.standard.subsystems.motor.SensorMotor;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SpinUpFlywheelDistance extends Command {
 	public final SensorMotor motor;
+	public final DistanceSensor distanceSensor;
 	public double motorSpeed;
 	protected final boolean endOnArrival;
 	
 	public SpinUpFlywheelDistance(boolean endOnArrival) {
 		motor = RobotMap.Component.flywheel;
 		motorSpeed = RobotMap.Constant.HumanInput.FLYWHEEL_TARGET_SPEED;
+		distanceSensor = RobotMap.Component.ultrasonicSensor;
 		requires(motor);
 		setInterruptible(true);
 		this.endOnArrival = endOnArrival;
 	}
 	
+	protected double calculatePower() {
+		return RobotMap.Constant.HumanInput.FLYWHEEL_TARGET_SPEED * distanceSensor.getDistance() / 200.0;
+	}
+	
 	@Override
 	public void initialize() {
-		motorSpeed = RobotMap.Constant.HumanInput.FLYWHEEL_TARGET_SPEED;
+		motorSpeed = calculatePower();
 		motor.enablePID();
 		motor.set(motorSpeed);
 	}
 	
 	@Override
 	public void execute() {
-		motorSpeed = RobotMap.Constant.HumanInput.FLYWHEEL_TARGET_SPEED;
+		motorSpeed = calculatePower();
 		motor.set(motorSpeed);
 	}
 	
